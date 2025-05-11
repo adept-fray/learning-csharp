263d009c12e5ca9cfa098381a102ed1ca98f76bb
adept-fray <siavash.kh41@gmail.com>
2025-05-11 20:06:33 +0200

    Create one method to get number input, to reuse. Used a while loop to continue the app until quit manually.

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,49 +1,70 @@
 ﻿Console.WriteLine("=== Calculator App ===");
 
-Console.WriteLine("Enter first number:");
-double firstNumber = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Choose the operator: +, -, *, /");
-string op = Console.ReadLine();
-
-Console.WriteLine("Enter second number:");
-double secondNumber = Convert.ToDouble(Console.ReadLine());
-
-
-double result = 0;
-bool valid = true;
-
-switch (op)
+double GetNumber()
 {
-    case "+":
-        result = firstNumber + secondNumber;
-        break;
-    case "-":
-        result = firstNumber - secondNumber;
-        break;
-    case "*":
-        result = firstNumber * secondNumber;
-        break;
-    case "/":
-        if(secondNumber == 0)
-        {
-            Console.WriteLine("Error: Divide by zero.");
-            valid = false;
-        }
-        else
-        {
-            result = firstNumber / secondNumber;
-        }
-            break;
-    default:
-        Console.WriteLine("Invalid operator.");
-        valid = false;
-        break;
+    return Convert.ToDouble(Console.ReadLine());
 }
 
-if (valid)
+while (true)
 {
-    Console.WriteLine($"Result: {result}");
+    Console.WriteLine("Enter first number:");
+    double firstNumber = GetNumber();
+
+    Console.WriteLine("Choose the operator: +, -, *, /");
+    string op = Console.ReadLine();
+
+    Console.WriteLine("Enter second number:");
+    double secondNumber = GetNumber();
+
+
+    double result = 0;
+    bool valid = true;
+
+    switch (op)
+    {
+        case "+":
+            result = firstNumber + secondNumber;
+            break;
+        case "-":
+            result = firstNumber - secondNumber;
+            break;
+        case "*":
+            result = firstNumber * secondNumber;
+            break;
+        case "/":
+            if (secondNumber == 0)
+            {
+                Console.WriteLine("Error: Divide by zero.");
+                valid = false;
+            }
+            else
+            {
+                result = firstNumber / secondNumber;
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid operator.");
+            valid = false;
+            break;
+    }
+
+    if (valid)
+    {
+        Console.WriteLine($"Result: {result}");
+    }
+
+    Console.WriteLine("Write q to exit.");
+    Console.WriteLine("Or press any key to continue");
+
+    char quit = Console.ReadKey().KeyChar;
+    if (quit == 'q' || quit == 'Q')
+    {
+        Console.WriteLine();
+        break;
+    }
+
+    Console.WriteLine();
 }
 
 Console.WriteLine("Program ended. Click any key to close.");
