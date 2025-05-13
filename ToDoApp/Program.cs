8224ee1c9e78266c20c0f6eba838f191b1c28975
adept-fray <siavash.kh41@gmail.com>
2025-05-13 16:15:44 +0200

    improved ToDoApp with method reuse, loop, and trycatch blocks.

--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -1,41 +1,75 @@
-﻿Console.WriteLine("=== ToDo App ===");
+﻿using System.ComponentModel;
 
-Console.WriteLine("You can add, list, and remove tasks.");
+Console.WriteLine("=== ToDo App ===");
 
-Console.WriteLine("you can write the commands without the numbers:");
-Console.WriteLine("1. add");
-Console.WriteLine("2. list");
-Console.WriteLine("3. remove");
-
-var  list = new List<string>();
-
-string command = Console.ReadLine();
-string add = "";
-int remove = 0;
-
-switch (command)
+void Menu()
 {
-    case "add":
-        Console.WriteLine("Write your task then hit enter to save it.");
-        string add = Console.ReadLine();
-        list.Add(add);
-        Console.WriteLine("Task added to the list.");
-        break;
-    case "list":
-        foreach (var item in list)
-        {
-            Console.WriteLine(item);
-        }
-        break;
-    case "remove":
-        Console.WriteLine("enter the number of the task you want to remove from the list.");
-        int remove = Convert.ToInt32(Console.ReadLine());
-        list.RemoveAt(remove);
-        Console.WriteLine($"task with index number of {remove} was removed from the list.");
-        break;
-    default:
-        Console.WriteLine($"{command} is not a correct command.");
-        break;
+    Console.WriteLine("Write 'q' to exit.");
+    Console.WriteLine("You can add, list, and remove tasks.");
+
+    Console.WriteLine("you can write the commands without the numbers:");
+    Console.WriteLine("1. add");
+    Console.WriteLine("2. list");
+    Console.WriteLine("3. remove");
+    Console.WriteLine("4. q");
 }
 
-Console.ReadKey();
+var  list = new List<string>();
+bool exitLoop = false;
+while (!exitLoop)
+{
+    Menu();
+
+    string command = Console.ReadLine();
+
+
+    switch (command)
+    {
+        case "add":
+            Console.WriteLine("Write your task then hit enter to save it.");
+            string add = Console.ReadLine();
+            if (string.IsNullOrEmpty(add))
+            {
+                Console.WriteLine("Can not add empty or space only text.");
+                break;
+            }
+            list.Add(add);
+            Console.WriteLine("Task added to the list.");
+            break;
+        case "list":
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            break;
+        case "remove":
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{list.IndexOf(item)}. {item}");
+            }
+            Console.WriteLine("enter the number of the task you want to remove from the list.");
+            try
+            {
+                int remove = int.Parse(Console.ReadLine());
+                list.RemoveAt(remove);
+                Console.WriteLine($"task with index number of {remove} was removed from the list.");
+            }
+            catch(IndexOutOfRangeException index)
+            {
+                Console.WriteLine("The number you entered does not exist in the list.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Could not remove try again.");
+            }
+            break;
+        case "q":
+            exitLoop = true;
+            break;
+        default:
+            Console.WriteLine($"{command} is not a correct command.");
+            break;
+    }
+}
+
+//Console.ReadKey();
